7c775af1945d6e1225000bdf4211b4c0785b8949
Eilert Sundt <eisundt@outlook.com>
2023-07-28 00:29:52 +0200

    create function playround
    playround computes who has won and returns a string with the result

--- javascript.js
+++ javascript.js
@@ -1,24 +1,42 @@
-let playerSelection;
-let computerSelection;
-
-
 function getComputerChoice() {
     let num = Math.random()
     if (num <= 0.33){
-        return "Rock"
+        return "rock"
     } else  if (num >= 0.66){
-        return "Paper"
+        return "paper"
     }else{
-        return "Scissors"
-    }
-}
-
-function playRound(playerSelection, computerSelection){
-    switch (playerSelection){
-        case{}:
+        return "scissors"
     }
 }
 
 function getPlayerChoice(){
-    prompt("Type either 'rock', 'paper' or 'scissors'")
+    input = prompt("Type either 'rock', 'paper' or 'scissors'").toLowerCase()
+    return input
 }
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection == "rock" && computerSelection == "rock"){
+        return "Its a draw"
+    }else if (playerSelection == "rock" && computerSelection == "paper"){
+        return "Computer wins: "+computerSelection+" beats"+playerSelection
+    }else if(playerSelection == "rock" && computerSelection == "scissors"){
+        return "You win: "+playerSelection+" beats "+computerSelection
+    }else if(playerSelection == "paper" && computerSelection == "rock"){
+        return "You win: paper beats rock"
+    }else if(playerSelection == "paper" && computerSelection == "paper"){
+        return "Its a draw"
+    }else if(playerSelection == "paper" && computerSelection == "scissors"){
+        return "Computer wins: scissors beats paper"
+    }else if(playerSelection == "scissors" && computerSelection == "rock"){
+        return"Computer wins: rock beats scissors"
+    }else if(playerSelection == "scissors" && computerSelection == "paper"){
+        return "You win: scissors beats paper"
+    }else if(playerSelection == "scissors" && computerSelection == "scissors"){
+        return "Its a draw" 
+    }
+}
+
+const playerSelection = getPlayerChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection))
