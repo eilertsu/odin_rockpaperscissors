5ebccc7602200b92e3a40cafb44e5e1d059753c6
Eilert Sundt <eisundt@outlook.com>
2023-07-28 01:59:52 +0200

    feat: make function game and getrounds
    makes it possible to input desired amount of rounds to play

--- javascript.js
+++ javascript.js
@@ -1,42 +1,56 @@
-function getComputerChoice() {
-    let num = Math.random()
-    if (num <= 0.33){
-        return "rock"
-    } else  if (num >= 0.66){
-        return "paper"
-    }else{
-        return "scissors"
+function game(){
+    for(i = 0; i < rounds; i++){
+        function getComputerChoice() {
+            let num = Math.random()
+            if (num <= 0.33){
+                return "rock"
+            } else  if (num >= 0.66){
+                return "paper"
+            }else{
+                return "scissors"
+            }
+        }
+        
+        function getPlayerChoice(){
+            input = prompt("Type either 'rock', 'paper' or 'scissors'").toLowerCase()
+            return input
+        }
+        
+        function playRound(playerSelection, computerSelection){
+            if (playerSelection == "rock" && computerSelection == "rock"){
+                return "Its a draw"
+            }else if (playerSelection == "rock" && computerSelection == "paper"){
+                return "Computer wins: paper beats rock"
+            }else if(playerSelection == "rock" && computerSelection == "scissors"){
+                return "You win: rock beats scissors"
+            }else if(playerSelection == "paper" && computerSelection == "rock"){
+                return "You win: paper beats rock"
+            }else if(playerSelection == "paper" && computerSelection == "paper"){
+                return "Its a draw"
+            }else if(playerSelection == "paper" && computerSelection == "scissors"){
+                return "Computer wins: scissors beats paper"
+            }else if(playerSelection == "scissors" && computerSelection == "rock"){
+                return"Computer wins: rock beats scissors"
+            }else if(playerSelection == "scissors" && computerSelection == "paper"){
+                return "You win: scissors beats paper"
+            }else if(playerSelection == "scissors" && computerSelection == "scissors"){
+                return "Its a draw" 
+            }
+        }
+        
+        const playerSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+
+        console.log(playRound(playerSelection, computerSelection))
     }
 }
 
-function getPlayerChoice(){
-    input = prompt("Type either 'rock', 'paper' or 'scissors'").toLowerCase()
+
+function getRounds() {
+    let input = prompt("How many rounds do you wish to play?")
     return input
 }
 
-function playRound(playerSelection, computerSelection){
-    if (playerSelection == "rock" && computerSelection == "rock"){
-        return "Its a draw"
-    }else if (playerSelection == "rock" && computerSelection == "paper"){
-        return "Computer wins: "+computerSelection+" beats"+playerSelection
-    }else if(playerSelection == "rock" && computerSelection == "scissors"){
-        return "You win: "+playerSelection+" beats "+computerSelection
-    }else if(playerSelection == "paper" && computerSelection == "rock"){
-        return "You win: paper beats rock"
-    }else if(playerSelection == "paper" && computerSelection == "paper"){
-        return "Its a draw"
-    }else if(playerSelection == "paper" && computerSelection == "scissors"){
-        return "Computer wins: scissors beats paper"
-    }else if(playerSelection == "scissors" && computerSelection == "rock"){
-        return"Computer wins: rock beats scissors"
-    }else if(playerSelection == "scissors" && computerSelection == "paper"){
-        return "You win: scissors beats paper"
-    }else if(playerSelection == "scissors" && computerSelection == "scissors"){
-        return "Its a draw" 
-    }
-}
+const rounds = getRounds()
 
-const playerSelection = getPlayerChoice();
-const computerSelection = getComputerChoice();
-
-console.log(playRound(playerSelection, computerSelection))
+console.log(game())
