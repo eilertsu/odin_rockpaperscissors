cf3e0b04adef56401f001287493e13ed1c26fd3e
Eilert Sundt <eisundt@outlook.com>
2023-08-09 01:16:21 +0200

    make game interactable with buttons

--- javascript.js
+++ javascript.js
@@ -1,49 +1,42 @@
-function startGame(){
-    for(i = 0; i < rounds; i++){
-        function getComputerChoice() {
-            let num = Math.random()
-            if (num <= 0.33){
-                return "rock"
-            } else  if (num >= 0.66){
-                return "paper"
-            }else{
-                return "scissors"
-            }
-        }
-        
-        function getPlayerChoice(){
-            input = prompt("Type either 'rock', 'paper' or 'scissors'").toLowerCase()
-            return input
-        }
-        
-        function playRound(playerSelection, computerSelection){
-            if (playerSelection == "rock" && computerSelection == "rock"){
-                return "Its a draw"
-            }else if (playerSelection == "rock" && computerSelection == "paper"){
-                return "Computer wins: paper beats rock"
-            }else if(playerSelection == "rock" && computerSelection == "scissors"){
-                return "You win: rock beats scissors"
-            }else if(playerSelection == "paper" && computerSelection == "rock"){
-                return "You win: paper beats rock"
-            }else if(playerSelection == "paper" && computerSelection == "paper"){
-                return "Its a draw"
-            }else if(playerSelection == "paper" && computerSelection == "scissors"){
-                return "Computer wins: scissors beats paper"
-            }else if(playerSelection == "scissors" && computerSelection == "rock"){
-                return"Computer wins: rock beats scissors"
-            }else if(playerSelection == "scissors" && computerSelection == "paper"){
-                return "You win: scissors beats paper"
-            }else if(playerSelection == "scissors" && computerSelection == "scissors"){
-                return "Its a draw" 
-            }
-        } 
-        
-        const playerSelection = getPlayerChoice();
-        const computerSelection = getComputerChoice();
+function getComputerChoice() {
+        let num = Math.random()
+        if (num <= 0.33){
+            return "rock"
+        } else  if (num >= 0.66){
+            return "paper"
+        }else{
+            return "scissors"
+}}
 
-        console.log(playRound(playerSelection, computerSelection))
-    }return ""
+function playRound(playerSelection, computerSelection){
+    if (playerSelection == "rock" && computerSelection == "rock"){
+        return "Its a draw"
+    }else if (playerSelection == "rock" && computerSelection == "paper"){
+        return "Computer wins: paper beats rock"
+    }else if(playerSelection == "rock" && computerSelection == "scissors"){
+        return "You win: rock beats scissors"
+    }else if(playerSelection == "paper" && computerSelection == "rock"){
+        return "You win: paper beats rock"
+    }else if(playerSelection == "paper" && computerSelection == "paper"){
+        return "Its a draw"
+    }else if(playerSelection == "paper" && computerSelection == "scissors"){
+        return "Computer wins: scissors beats paper"
+    }else if(playerSelection == "scissors" && computerSelection == "rock"){
+        return"Computer wins: rock beats scissors"
+    }else if(playerSelection == "scissors" && computerSelection == "paper"){
+        return "You win: scissors beats paper"
+    }else if(playerSelection == "scissors" && computerSelection == "scissors"){
+        return "Its a draw" 
+    }
 }
 
 
-let rounds;
+const buttons = document.querySelectorAll("button")
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () =>{
+        playerSelection = button.className
+        computerSelection = getComputerChoice()
+        console.log(playRound(playerSelection, computerSelection))
+    })
+});
